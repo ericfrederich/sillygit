bbbbbbbe4883ed9113da954b84bc8faedf7f55db
Eric L Frederich <eric.frederich@gmail.com>
2015-08-11 19:18:47 -0400

    support parallel processing

--- commit.py
+++ commit.py
@@ -8,6 +8,54 @@ import time
 import hashlib
 import itertools
 from datetime import datetime
+import multiprocessing
+import Queue
+
+def finder(stop_queue, results_queue, start_time, time_delta, template, hsh):
+    new_time = start_time - time_delta
+    found = False
+    tries = 0
+    before = datetime.now()
+    while not found:
+        # drift time as needed
+        # though this shouldn't happen too often since
+        # the inner loop generates 41,478,481 tries
+        new_time += time_delta
+        content = template % {'TIME': new_time}
+
+        print 'trying with time', new_time
+
+        for padding in white_noise_generator():
+            # keep track of and print tries
+            tries += 1
+
+            if tries % 10000 == 0:
+                try:
+                    stop_queue.get_nowait()
+                except Queue.Empty:
+                    pass
+                else:
+                    print 'got the stop signal'
+                    results_queue.put(tries)
+
+            # if tries % 10000 == 0:
+            #     print tries, 'tries', '(%d%%)' % int(100.0 * tries / 16**len(hsh))
+
+            # calculate sha
+            header = 'commit %d\0' % len(content + padding)
+            store = header + content + padding
+            h = hashlib.sha1()
+            h.update(store)
+            sha = h.hexdigest()
+
+            # break if we found one that ends with the desired hash
+            if sha.endswith(hsh):
+                found = True
+                break
+
+    after = datetime.now()
+
+    results_queue.put(( before, after, sha, store, tries, new_time, content, padding ))
 
 def run_command(cmd, stdin=None, allowed_exit_codes=[0]):
     """
@@ -40,7 +88,7 @@ def white_noise_generator(length=4, width=80):
             ret += '\n'
             yield ret
 
-def commit(git_dir, add, hsh, msg):
+def commit(git_dir, add, hsh, msg, n_procs):
     print 'creating commit for'
     print '  ', git_dir
     print '  ', hsh
@@ -65,7 +113,7 @@ def commit(git_dir, add, hsh, msg):
     print 'tree hash  ', tree_hash
     print 'parent hash', parent_hash
 
-    TEMPLATE='''\
+    template = '''\
 tree %(TREE)s
 parent %(PARENT)s
 author %(USERNAME)s <%(EMAIL)s> %(TIME)s -0400
@@ -81,38 +129,38 @@ committer %(USERNAME)s <%(EMAIL)s> %(TIME)s -0400
         'EMAIL': email,
     }
 
-    start = int(time.time())
-    time_delta = -1
-    found = False
-    tries = 0
-    before = datetime.now()
-    while not found:
-        # drift time as needed
-        # though this shouldn't happen too often since
-        # the inner loop generates 41,478,481 tries
-        time_delta += 1
-        content = TEMPLATE % {'TIME': start + time_delta}
+    start_time = int(time.time())
 
-        for padding in white_noise_generator():
-            # keep track of and print tries
-            tries += 1
-            if tries % 10000 == 0:
-                print tries, 'tries', '(%d%%)' % int(100.0 * tries / 16**len(hsh))
+    # create some queues for communication
+    results_queue = multiprocessing.Queue()
+    stop_queue = multiprocessing.Queue()
 
-            # calculate sha
-            header = 'commit %d\0' % len(content + padding)
-            store = header + content + padding
-            h = hashlib.sha1()
-            h.update(store)
-            sha = h.hexdigest()
+    # create all the processes using an offset for the start time so they're unique
+    procs = []
+    for i in range(n_procs):
+        proc = multiprocessing.Process(target=finder, args=(stop_queue, results_queue, start_time + i, n_procs, template, hsh))
+        procs.append(proc)
 
-            # break if we found one that ends with the desired hash
-            if sha.endswith(hsh):
-                found = True
-                break
+    # start all processes
+    for proc in procs:
+        proc.start()
 
+    # first thing back on the results queue will alaways be the result
+    before, after, sha, store, tries, new_time, content, padding = results_queue.get()
 
-    after = datetime.now()
+    # signal all the other processes that we're done
+    for i in range(n_procs):
+        stop_queue.put(None)
+
+    # now everything on the results queue is just the number of tries from each process
+    # do a blocking call because we know how many processes there were
+    for i in range(n_procs - 1):
+        tries += results_queue.get()
+
+    # kill all the processes though they should be done already
+    for proc in procs:
+        proc.terminate()
+
     print '*' * 80
     print sha
     print repr(store)
@@ -120,7 +168,7 @@ committer %(USERNAME)s <%(EMAIL)s> %(TIME)s -0400
     print 'elapsed:', after - before
     print 'tries  :', tries
     print '%r tries per second' % (tries / (after - before).total_seconds())
-    print 'had to increment commit time by', time_delta, 'seconds'
+    print 'had to increment commit time by', new_time - start_time, 'seconds'
 
     commit_hash = run_command(git_cmd + ['hash-object', '-t', 'commit', '-w', '--stdin'], stdin=content + padding).strip()
     if commit_hash == sha:
@@ -135,11 +183,12 @@ def main():
     parser.add_argument('--add', '-a', action='store_true')
     parser.add_argument('--message', '-m')
     parser.add_argument('--git-dir')
+    parser.add_argument('--parallel', type=int, default=1)
     parser.add_argument('hash')
     args = parser.parse_args()
     # if len(args.hash) > 4:
     #     raise ValueError('hash too big, only 4 supported')
-    commit(args.git_dir, args.add, args.hash, args.message)
+    commit(args.git_dir, args.add, args.hash, args.message, args.parallel)
 
 if __name__ == '__main__':
     main()
