111111120cd5a6b4fcf9b5f98cdef48227cc51fc
Eric L Frederich <eric.frederich@gmail.com>
2015-08-11 18:48:36 -0400

    more work

--- commit.py
+++ commit.py
@@ -6,12 +6,17 @@ import time
 import hashlib
 import itertools
 
-def run_command(cmd, allowed_exit_codes=[0]):
+def run_command(cmd, stdin=None, allowed_exit_codes=[0]):
     """
     wrapper around subprocess.Popen
     returns stdout, stderr and the return code
     """
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    if stdin:
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        p.stdin.write(stdin)
+    else:
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+
     out, err = p.communicate()
     ret = p.wait()
     if allowed_exit_codes is not None and ret not in allowed_exit_codes:
@@ -33,8 +38,13 @@ def commit(git_dir, hsh, msg):
     tree_hash = out.strip()
     print 'commit hash is', tree_hash
 
+    out, err, ret = run_command(['git', '--git-dir', git_dir, 'rev-parse', 'HEAD'])
+    parent_hash = out.strip()
+    print 'parent hash is', parent_hash
+
     TEMPLATE='''\
 tree %(TREE)s
+parent %(PARENT)s
 author Eric L Frederich <eric.frederich@gmail.com> %(TIME)s -0400
 committer Eric L Frederich <eric.frederich@gmail.com> %(TIME)s -0400
 
@@ -43,6 +53,7 @@ committer Eric L Frederich <eric.frederich@gmail.com> %(TIME)s -0400
         'TREE': tree_hash,
         'TIME': '%(TIME)s',
         'MESSAGE': msg,
+        'PARENT': parent_hash,
     }
 
     start = int(time.time())
@@ -60,6 +71,8 @@ committer Eric L Frederich <eric.frederich@gmail.com> %(TIME)s -0400
             for n in padding:
                 content += '\n' + (' ' * n)
 
+            content += '\n'
+
             header = 'commit %d\0' % len(content)
             store = header + content
             h = hashlib.sha1()
@@ -67,16 +80,23 @@ committer Eric L Frederich <eric.frederich@gmail.com> %(TIME)s -0400
             sha = h.hexdigest()
             # print 'would be', sha
             if sha.endswith(hsh):
-                print 'after', tries, 'tries'
-                print 'had to increment time by', time_delta, 'seconds or', time_delta / 60.0, 'minutes'
                 found = True
                 break
 
+    print 'after', tries, 'tries'
+    print 'had to increment time by', time_delta, 'seconds or', time_delta / 60.0, 'minutes'
     print 'sha...', sha
-    print 'content...'
+    print repr(store)
     print repr(content)
-    print content
 
+    out, err, ret = run_command(['git', '--git-dir', git_dir, 'hash-object', '-t', 'commit', '-w', '--stdin'], stdin=content)
+    commit_hash = out.strip()
+    if commit_hash == sha:
+        print 'WOO HOO'
+    else:
+        raise RuntimeError('unexpected hash')
+
+    run_command(['git', '--git-dir', git_dir, 'update-ref', 'HEAD', sha])
 
 def main():
     parser = argparse.ArgumentParser()
